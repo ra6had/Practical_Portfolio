d69ded876ac45df5facc2d81eae623b480ff5f1c
Rashad <46797873+ra6had@users.noreply.github.com>
2019-02-03 23:23:04 +0300

    Update model.py
    Shrinking Code 2 v.1: includes several lines for testing purposes that will be removed from the final version

--- model.py
+++ model.py
@@ -1,87 +1,85 @@
 # Project Name: Building an Agent Based Model
 # Author: Rashad a.k. Ahmed
-# Version: Shrinking Code I (23.01.2019)
+# Version: Shrinking Code 2 v.1 (01.02.2019)
+
+
+# This code generates 100 simple agents represented by coordinates 
+# between 0 and 99 in 2D space.
+# The agents take 100 steps in random directions and their final positions 
+# are plotted.
+# This version includes several commented out lines that may be useful
+# for testing the functionality of the code
 
 
 
 import random
-import operator
+#import operator
 import matplotlib.pyplot
+import copy
+
+# Initializing number of agents and iterations
+num_of_agents = 10
+num_of_iterations = 100 
 
 # Initializing agents list
 agents = []
 
-# Initializing first agent's position and appending it to agents list.
-agents.append([random.randint(0,99), random.randint(0,99)])
+# populating the agents list
+for i in range(num_of_agents):
+    agents.append([random.randint(0,100),random.randint(0,100)])
+
+# Uncomment the below lines to print/plot the initial positions of agents
+#print("Agents' coordinates= " + str(agents))
+#
+#for i in range(len(agents)):
+#    for k in range(2):
+#        matplotlib.pyplot.scatter(agents[i][1], agents[i][0])
+
+#matplotlib.pyplot.show() 
+        
+ 
+# copy the agents list to a new list to be used for comparison later
+agents2 = copy.deepcopy(agents) 
+
+# Make all agents take "num_of_iteration" number of steps in random directions
+# Such that the iteration runs through all the agents one step at a time
+# Using a pac-man approach to handle edges
+for i in range(num_of_iterations):    
+    for j in range(num_of_agents):  
+        for k in range(len(agents2[1])):
+            if random.random() < 0.5:
+                agents2[j][k] = (agents2[j][k] + 1) % 100
+            else:
+                agents2[j][k] = (agents2[j][k] - 1) % 100
+#    print("agents = " + str(agents)) # For testing, reduce the num_of_iterations
 
 
-# Random walk one step.
-if random.random() < 0.5: # motion in the y-axis
-    agents[0][0] += 1
-else:
-    agents[0][0] -= 1
+# Print the coordinates of agents
+print("Agents' coordinates= " + str(agents2))
 
-if random.random() < 0.5: # motion in the x-axis
-    agents[0][1] += 1
-else:
-    agents[0][1] -= 1
-
-    
-# Random walk a second step
-if random.random() < 0.5:
-    agents[0][0] += 1
-else:
-    agents[0][0] -= 1
-
-if random.random() < 0.5:
-    agents[0][1] += 1
-else:
-    agents[0][1] -= 1
-
-
-# Repeating the above steps for a second agent.
-agents.append([random.randint(0,99), random.randint(0,99)])
-
-
-# Random walk one step.
-if random.random() < 0.5:
-    agents[1][0] += 1
-else:
-    agents[1][0] -= 1
-
-if random.random() < 0.5:
-    agents[1][1] += 1
-else:
-    agents[1][1] -= 1
-    
-
-# Random walk a second step
-if random.random() < 0.5:
-    agents[1][0] += 1
-else:
-    agents[1][0] -= 1
-
-if random.random() < 0.5:
-    agents[1][1] += 1
-else:
-    agents[1][1] -= 1
-    
 
 
 
 # Compute and distance between agents
-dist = (((agents[0][0] - agents[1][0])**2) + ((agents[0][1] - agents[1][1])**2))**0.5
-print(agents)
-print(dist)
+#dist = (((agents[0][0] - agents[1][0])**2) + ((agents[0][1] - agents[1][1])**2))**0.5
+#print(agents)
+#print(dist)
 
 # Print northernmost and easternmost agents
-print(max(agents))
-print(max(agents, key=operator.itemgetter(1)))
+#print('Northernmost agent = ' + str(max(agents2)))
+#print('Easternmost agent = ' + str(max(agents2, key=operator.itemgetter(1))))
 
 
-# Plotting the agents
-matplotlib.pyplot.ylim(0,99)
-matplotlib.pyplot.xlim(0,99)
-matplotlib.pyplot.scatter(agents[0][1], agents[0][0], color='red')
-matplotlib.pyplot.scatter(agents[1][1], agents[1][0], color='blue')
+# Plotting the final position of the agents
+for i in range(len(agents2)):
+    matplotlib.pyplot.scatter(agents2[i][1], agents2[i][0])
+       
 matplotlib.pyplot.show()
+
+print(agents)
+print(agents2)
+
+# compute and print the components of the displacement
+# vector for each agent in the 2 dimension
+for i in range(len(agents)):
+    print([str((agents2[i][0])-(agents[i][0])) +"," +str((agents2[i][1])-(agents[i][1]))])
