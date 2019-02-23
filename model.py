47e2b02909be3dbbd2638043d426f918e0d9c406
Rashad <46797873+ra6had@users.noreply.github.com>
2019-02-23 15:02:05 +0300

    Update model.py
    Building Tools tutorial including challenge 1 & 2. Without proper docummentation

--- model.py
+++ model.py
@@ -11,6 +11,25 @@
 
 import random
 import matplotlib.pyplot
+import time
+
+
+start = time.clock()
+
+#def distance_between(agents_row_a, agents_row_b):
+#    return (((agents_row_a[0] - agents_row_b[0])**2) + ((agents_row_a[1] - agents_row_b[1]) **2))**0.5
+
+def distances(agents):
+    global distance_set
+    distance_set = []
+    i = 0
+    for i in range(len(agents)):
+        for j in range(len(agents)):
+            if j > i:
+                distance = (((agents[i][0] - agents[j][0])**2 + ((agents[i][1] - agents[j][1])**2))**0.5)
+                distance_set.append(distance)
+    return distance_set
+    
 
 # Initializing number of agents and iterations
 num_of_agents = 10
@@ -35,7 +54,7 @@ for i in range(num_of_iterations):
                 agents[j][k] = (agents[j][k] - 1) % 100
  
 # Print the final coordinates of agents
-print("Agents' coordinates= " + str(agents))
+print("Agents' coordinates= " + str(agents) + '\n')
 
 
 # Plot the final position of agents
@@ -43,3 +62,14 @@ for i in range(len(agents)):
     matplotlib.pyplot.scatter(agents[i][1], agents[i][0])
        
 matplotlib.pyplot.show()
+
+distance = distances(agents)
+print('\n')
+print(distance, '\n')
+
+print('the number of distances = ' + str(len(distance_set)) + '\n')
+print('Maximum distance = ' +str(max(distance_set)) + '\n')
+print('Minimum distance = ' + str(min(distance_set)) + '\n')
+
+end = time.clock()
+print('time = ' + str(end - start) + '\n')
